ce908b59ded322993a5564e4012cbc4ce8924dee
KAIQUE ONENCIO DE SOUZA <0201392321040>
2025-09-24 09:39:36 -0300

    getOne, updateParcial e updateCompleto

--- 05_Projeto-To-Do/ToDo_Vania/Backend/Controllers/TarefaController.js
+++ 05_Projeto-To-Do/ToDo_Vania/Backend/Controllers/TarefaController.js
@@ -69,4 +69,94 @@ export default class tarefaController{
             res.status(500).json({message:"Erro ao buscar todas as tarefas", error});
         }
     }
+
+    static async getOne(req, res){
+        try {
+            const id = req.params.id;
+            const ObjectId = Types.ObjectId;
+
+            if (!ObjectId.isValid(id))
+            {
+                return res.status(422).json({message: "Id inválido"});
+            }
+
+            const tarefa = await Tarefa.findById(id);
+
+            if (!tarefa)
+            {
+                return res.status(404).json({message: "Tarefa não encontrada"})
+            }
+
+            res.status(200).json({tarefa});
+        } catch (error) {
+            res.status(500).json({message: "Erro ao buscar uma tarefa", error});
+        }
+    }
+
+    static async updateParcial(req, res){
+        try {
+            const id = req.params.id;
+            const {situacao} = req.body;
+            const ObjectId = Types.ObjectId;
+
+            if (!ObjectId.isValid(id))
+            {
+                return res.status(422).json({message: "Id inválido"});
+            }
+
+            if (!situacao)
+            {
+                return res.status(422).json({message: "Situação não informada"});
+            }
+
+            const updatedTarefa = await Tarefa.findByIdAndUpdate(id, {situacao}, {new: true}); // o 'new: true' faz a alteração e retorna o resultado atualizado
+
+            if (!updatedTarefa)
+            {
+                return res.status(404).json({message: "Tarefa não encontrada"})
+            }
+
+            return res.status(200).json({message: "Situação alterada com sucesso", updatedTarefa});
+        } catch (error) {
+            return res.status(500).json({message: "Erro ao buscar uma tarefa", error});
+        }
+    }
+
+    static async updateCompleto(req, res){
+        try {
+            const id = req.params.id;
+            const {titulo, descricao, dataLimite, situacao} = req.body;
+            const ObjectId = Types.ObjectId;
+
+            if (!ObjectId.isValid(id))
+            {
+                return res.status(422).json({message: "Id inválido"});
+            }
+
+            if (!titulo || !descricao || !dataLimite || !situacao)
+            {
+                return res.status(422).json({message: "Todos os dados da tarefa são obrigatórios"});
+            }
+
+            const updateData = {
+                titulo,
+                descricao,
+                dataLimite: new Date(dataLimite + "T00:00:00"),
+                situacao
+            }
+
+            // updateData["descricao"] = descricao
+
+            const updatedTarefa = await Tarefa.findByIdAndUpdate(id, updateData, {new: true}); // o 'new: true' faz a alteração e retorna o resultado atualizado
+
+            if (!updatedTarefa)
+            {
+                return res.status(404).json({message: "Tarefa não encontrada"})
+            }
+
+            return res.status(200).json({message: "Tarefa alterada com sucesso", updatedTarefa});
+        } catch (error) {
+            return res.status(500).json({message: "Erro ao buscar uma tarefa", error});
+        }
+    }
 }//class
--- 05_Projeto-To-Do/ToDo_Vania/Backend/Routes/routes.js
+++ 05_Projeto-To-Do/ToDo_Vania/Backend/Routes/routes.js
@@ -6,6 +6,8 @@ const routes = Router();
 routes.post("/Create", TarefaController.create);
 routes.get("/getAll", TarefaController.getAll);
 routes.delete("/:id", TarefaController.remove);
-//routes.get("/:id", TarefaController.getOne);
+routes.get("/:id", TarefaController.getOne);
+routes.put("/:id", TarefaController.updateCompleto);
+routes.patch("/:id", TarefaController.updateParcial);
 
 export default routes;
